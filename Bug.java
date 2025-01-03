6732695c29f25470e8f12538794687b2c01405eb
BugHunter-X <askzenai@gmail.com>
2025-01-03 09:04:04 +0100

    Add Bug.java file

---
+++ Bug.java
@@ -0,0 +1,13 @@
+public class MyClass {
+    public static void main(String[] args) {
+        int i = 0;
+        while (i < 10) {
+            System.out.println(i);
+            if (i == 5) {
+                // Forgot to increment i
+                break;
+            }
+            i++;
+        }
+    }
+}
